de98217dfbd03bfa6fae5a0d33ae8238d21274e4
Domenico DiNicola <dom.dinicola@gmail.com>
2023-09-27 09:33:52 -0600

    fix cancel

--- src/hope_payment_gateway/apps/fsp/western_union/endpoints/cancel.py
+++ src/hope_payment_gateway/apps/fsp/western_union/endpoints/cancel.py
@@ -39,31 +39,31 @@ def cancel_request(record_code, mtcn, database_key, reason=WIC):
 
 
 def cancel(record_uuid, mtcn):
-    log = PaymentRecord.objects.get(uuid=record_uuid)
-    response = search_request(log.record_code, mtcn)
+    pr = PaymentRecord.objects.get(uuid=record_uuid)
+    response = search_request(pr.record_code, mtcn)
     payload = response["content"]
     try:
         database_key = payload["payment_transactions"]["payment_transaction"][0]["money_transfer_key"]
     except TypeError:
         database_key = None
     if not database_key:
-        log.message = "Search Error: No Money Transfer Key"
-        log.success = False
-        log.fail()
-        log.save()
-        return log
+        pr.message = "Search Error: No Money Transfer Key"
+        pr.success = False
+        pr.fail()
+        pr.save()
+        return pr
 
-    response = cancel_request(log.record_code, mtcn, database_key)
+    response = cancel_request(pr.record_code, mtcn, database_key)
     extra_data = {"db_key": database_key, "mtcn": mtcn}
 
     if response["code"] == 200:
-        log.message = "Cancelled"
-        log.success = True
-        log.cancel()
+        pr.message = "Cancelled"
+        pr.success = True
+        pr.cancel()
     else:
-        log.message = "Cancel error"
-        log.success = False
-        log.store()
-    log.extra_data.update(extra_data)
-    log.save()
-    return log
+        pr.message = "Cancel error"
+        pr.success = False
+        pr.fail()
+    pr.extra_data.update(extra_data)
+    pr.save()
+    return pr
