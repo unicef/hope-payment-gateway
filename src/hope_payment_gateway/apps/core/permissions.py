d58c3101ab5e31bb742c839adf81f36f320e6072
Domenico DiNicola <dom.dinicola@gmail.com>
2024-01-08 16:32:15 +0100

    fix ip whitelist

--- src/hope_payment_gateway/apps/core/permissions.py
+++ src/hope_payment_gateway/apps/core/permissions.py
@@ -4,11 +4,19 @@ from constance import config
 from rest_framework import permissions
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+    return ip
+
+
 class WhitelistPermission(permissions.BasePermission):
     """
     Global permission check for whitelisted IPs.
     """
 
     def has_permission(self, request, view):
-        domain = request.META.get("REMOTE_HOST", None)
-        return domain in config.WESTERN_UNION_WHITELISTED_IPS.split(";") or settings.DEBUG
+        return get_client_ip(request) in config.WESTERN_UNION_WHITELISTED_IPS.split(";") or settings.DEBUG
