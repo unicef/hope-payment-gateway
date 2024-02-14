f38a891005283f6effa30f5056871b723137cb84
Domenico DiNicola <dom.dinicola@gmail.com>
2024-02-14 18:02:59 +0100

    config model

--- src/hope_payment_gateway/apps/fsp/western_union/handlers.py
+++ src/hope_payment_gateway/apps/fsp/western_union/handlers.py
@@ -1,4 +1,5 @@
 from hope_payment_gateway.apps.fsp.western_union.endpoints.send_money import send_money
+from hope_payment_gateway.apps.gateway.models import FinancialServiceProvider, FinancialServiceProviderConfig
 from hope_payment_gateway.apps.gateway.registry import FSPProcessor
 
 
@@ -6,3 +7,11 @@ class WesternUnionHandler(FSPProcessor):
     def notify(self, records):
         for record in records:
             send_money(record.get_payload())
+
+    def get_configuration(self, config_key):
+        wu = FinancialServiceProvider.objects.get(vision_vendor_number="1900723202")
+        try:
+            config = FinancialServiceProviderConfig.objects.get(key=config_key, fsp=wu).configuration
+        except FinancialServiceProviderConfig.DoesNotExist:
+            config = wu.configuration
+        return config
