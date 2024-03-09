2e84ea41aed954155cd138da185307619e01cfc3
Domenico DiNicola <dom.dinicola@gmail.com>
2024-03-09 09:49:20 +0100

    key errors

--- src/hope_payment_gateway/apps/fsp/western_union/endpoints/nis.py
+++ src/hope_payment_gateway/apps/fsp/western_union/endpoints/nis.py
@@ -11,6 +11,7 @@ from rest_framework_xml.renderers import XMLRenderer
 from zeep.exceptions import ValidationError
 
 from hope_payment_gateway.apps.fsp.western_union.endpoints.client import WesternUnionClient
+from hope_payment_gateway.apps.fsp.western_union.endpoints.exceptions import InvalidRequest
 from hope_payment_gateway.apps.gateway.models import PaymentRecord
 
 SUCCESS = "BIS003"
@@ -27,23 +28,43 @@ class WesternUnionApi(APIView):
 
 
 class NisNotificationView(WesternUnionApi):
-    def get(self, request):
-        payload = {"western_union": "this is 200, not an error"}
-        # request.data["{http://schemas.xmlsoap.org/soap/envelope/}Body"][
-        #     "{http://www.westernunion.com/schema/xrsi}nis-notification-request"
-        # ]
+    @staticmethod
+    def get_payload(request):
+        header = "{http://schemas.xmlsoap.org/soap/envelope/}Body"
+        secondary_header = "{http://www.westernunion.com/schema/xrsi}nis-notification-request"
 
+        try:
+            return request.data[header][secondary_header]
+        except KeyError:
+            if header in request.data:
+                key = secondary_header
+                keys = ", ".join(request.data[header].keys())
+            else:
+                key = header
+                keys = ", ".join(request.data.keys())
+            raise InvalidRequest(f"header {key} not in {keys}")
+
+    def get(self, request):
+        try:
+            payload = self.get_payload(request)
+        except InvalidRequest as e:
+            return Response(
+                {"invalid_request": e},
+                status=HTTP_400_BAD_REQUEST,
+            )
         return Response(payload)
 
     def post(self, request):
-        payload = request.data["{http://schemas.xmlsoap.org/soap/envelope/}Body"][
-            "{http://www.westernunion.com/schema/xrsi}nis-notification-request"
-        ]
+        try:
+            payload = self.get_payload(request)
+        except InvalidRequest as e:
+            return Response(
+                {"invalid_request": e},
+                status=HTTP_400_BAD_REQUEST,
+            )
         fsp_code = payload["transaction_id"]
-
         mtcn = payload["money_transfer_control"]["mtcn"]
         notification_type = payload["notification_type"]
-
         delivered_quantity = payload["payment_details"]["origination"]["principal_amount"]
 
         try:
@@ -80,18 +101,17 @@ class NisNotificationView(WesternUnionApi):
         pr.save()
 
         try:
-            resp = nic_acknowledge(payload)
+            resp = self.nic_acknowledge(payload)
         except ValidationError as exp:
             return Response({"validation_error": str(exp)}, status=HTTP_400_BAD_REQUEST)
         return HttpResponse(resp, content_type="application/xml")
 
+    def nic_acknowledge(self, payload):
+        for tag_name in ["message_code", "message_text", "reason_code", "reason_desc"]:
+            payload.pop(tag_name, None)
+        payload["ack_message"] = "Acknowledged"
 
-def nic_acknowledge(payload):
-    for tag_name in ["message_code", "message_text", "reason_code", "reason_desc"]:
-        payload.pop(tag_name, None)
-    payload["ack_message"] = "Acknowledged"
-
-    client = WesternUnionClient("NisNotification.wsdl")
-    node = client.prepare("NotifServiceReply", payload)
-    data = etree.tostring(node, pretty_print=True, with_tail=True).decode()
-    return data
+        client = WesternUnionClient("NisNotification.wsdl")
+        node = client.prepare("NotifServiceReply", payload)
+        data = etree.tostring(node, pretty_print=True, with_tail=True).decode()
+        return data
