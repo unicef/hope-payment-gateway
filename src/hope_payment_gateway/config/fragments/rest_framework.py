d3cfd4f779fc0f27a94587d3e17c3944bb3e252c
Domenico <dom.dinicola@gmail.com>
2024-05-02 15:27:39 +0200

    fragments (#50)

---
+++ src/hope_payment_gateway/config/fragments/rest_framework.py
@@ -0,0 +1,42 @@
+import datetime
+
+from hope_payment_gateway.config.settings import SECRET_KEY
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Payment Gateway API",
+    "DESCRIPTION": "Payment Gateway to integrate HOPE with FSP",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+    "SWAGGER_UI_DIST": "SIDECAR",
+    "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+    "REDOC_DIST": "SIDECAR",
+}
+
+JWT_AUTH = {
+    "JWT_VERIFY": False,  # this requires private key
+    "JWT_VERIFY_EXPIRATION": True,
+    "JWT_LEEWAY": 60,
+    "JWT_EXPIRATION_DELTA": datetime.timedelta(seconds=30000),
+    "JWT_AUDIENCE": None,
+    "JWT_ISSUER": None,
+    "JWT_ALLOW_REFRESH": False,
+    "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=7),
+    "JWT_AUTH_HEADER_PREFIX": "JWT",
+    "JWT_SECRET_KEY": SECRET_KEY,
+    "JWT_DECODE_HANDLER": "rest_framework_jwt.utils.jwt_decode_handler",
+    # Keys will be set in core.apps.Config.ready()
+    "JWT_PUBLIC_KEY": "?",
+    # 'JWT_PRIVATE_KEY': wallet.get_private(),
+    # 'JWT_PRIVATE_KEY': None,
+    "JWT_ALGORITHM": "RS256",
+}
---
+++ src/hope_payment_gateway/config/fragments/sentry.py
@@ -0,0 +1,18 @@
+import sentry_sdk
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
+
+from .. import env
+
+SENTRY_DSN = env("SENTRY_DSN", default=None)  # noqa: F405
+
+if SENTRY_DSN:  # pragma: no cover
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        # by default this is False, must be set to True so the library attaches the request data to the event
+        send_default_pii=True,
+        enable_tracing=True,
+        integrations=[DjangoIntegration(), CeleryIntegration(), LoggingIntegration()],
+        environment=env("SENTRY_ENVIRONMENT", default=None),
+    )
