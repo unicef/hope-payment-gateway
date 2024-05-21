a96a26e7ae613e69e71d87a7f45d473797f91746
Domenico DiNicola <dom.dinicola@gmail.com>
2024-05-21 14:12:33 +0200

    whitelist :

--- src/hope_payment_gateway/apps/core/permissions.py
+++ src/hope_payment_gateway/apps/core/permissions.py
@@ -7,7 +7,7 @@ from rest_framework import permissions
 def get_client_ip(request):
     x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
-        ip = x_forwarded_for.split(",")[0]
+        ip = x_forwarded_for.split(",")[0].split(":")[0]
     else:
         ip = request.META.get("REMOTE_ADDR")
     return ip
