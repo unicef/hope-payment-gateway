4f6a2fe1109ff628fcd4be8c2589b97d358aebe4
Domenico DiNicola <dom.dinicola@gmail.com>
2025-01-02 13:03:41 -0600

    actions

--- src/hope_payment_gateway/apps/fsp/moneygram/apps.py
+++ src/hope_payment_gateway/apps/fsp/moneygram/apps.py
@@ -1,9 +1,14 @@
 from django.apps import AppConfig as BaseAppConfig
 
+from hope_payment_gateway.apps.gateway.registry import registry
+
 
 class AppConfig(BaseAppConfig):
     name = __name__.rpartition(".")[0]
     verbose_name = "MoneyGram"
 
     def ready(self) -> None:
+        from .handlers import MoneyGramHandler
+
+        registry.register(MoneyGramHandler)
         from . import tasks  # noqa
