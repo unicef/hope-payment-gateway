787dbc559541a242b16a54cefe20b9b7c4e0a2b2
Domenico DiNicola <dom.dinicola@gmail.com>
2025-04-11 17:12:20 -0600

    refactoring

--- src/hope_payment_gateway/apps/fsp/moneygram/tasks.py
+++ src/hope_payment_gateway/apps/fsp/moneygram/tasks.py
@@ -1,13 +1,9 @@
-import logging
-
 from constance import config
 from strategy_field.utils import fqn
 
-from hope_payment_gateway.apps.core.tasks import lock_job
 from hope_payment_gateway.apps.fsp.moneygram.client import MoneyGramClient
+from hope_payment_gateway.apps.fsp.tasks_utils import send_to_fsp, notify_records_to_fsp
 from hope_payment_gateway.apps.gateway.models import (
-    AsyncJob,
-    PaymentInstruction,
     PaymentInstructionState,
     PaymentRecord,
     PaymentRecordState,
@@ -16,58 +12,29 @@ from hope_payment_gateway.config.celery import app
 
 
 def moneygram_notify(to_process_ids: list[PaymentRecord]) -> None:
-    client = MoneyGramClient()
-    for record in PaymentRecord.objects.select_related("parent__fsp").filter(id__in=to_process_ids):
-        client.create_transaction(record.get_payload())
+    notify_records_to_fsp(fqn(MoneyGramClient), to_process_ids)
 
 
 @app.task()  # queue="executors"
 def moneygram_send_money(tag=None, threshold=10000):
     """Task to trigger MoneyGram payments."""
-    logging.info("MoneyGram Task started")
+    fsp = "MoneyGram"
+    fsp_vendor_number = config.MONEYGRAM_VENDOR_NUMBER
     threshold = threshold or config.MONEYGRAM_THREASHOLD
+    action_fqn = moneygram_notify
+    group_key = "mg-send-instruction"
 
-    records_count = 0
-
-    qs = PaymentInstruction.objects.filter(
-        status=PaymentInstructionState.READY, fsp__vendor_number=config.MONEYGRAM_VENDOR_NUMBER, active=True
-    )
-    if tag:
-        qs = qs.filter(tag=tag)
-
-    for pi in qs:
-        logging.info(f"Processing payment instruction {pi.external_code}")
-        records = pi.paymentrecord_set.filter(status=PaymentRecordState.PENDING)
-        records_count += records.count()
-        if records_count > threshold:
-            break
-
-        logging.info(f"Sending {records_count} records {pi} to MoneyGram")
-        records_ids = list(records.values_list("id", flat=True))
-        job = AsyncJob.objects.create(
-            description="Send Instruction to MoneyGram",
-            type=AsyncJob.JobType.STANDARD_TASK,
-            action=fqn(moneygram_notify),
-            config={"to_process_ids": records_ids},
-            instruction=pi,
-            group_key="mg-send-instruction",
-        )
-        with lock_job(job):
-            job.queue()
-            pi.status = PaymentInstructionState.PROCESSED
-            pi.save()
-
-    logging.info("MoneyGram Task completed")
+    send_to_fsp(fsp, fsp_vendor_number, action_fqn, group_key, threshold, tag)
 
 
 @app.task
 def moneygram_update(ids=None) -> None:
+    client = MoneyGramClient()
     qs = PaymentRecord.objects.select_related("parent__fsp").filter(
         parent__fsp__vendor_number=config.MONEYGRAM_VENDOR_NUMBER,
         parent__status=PaymentInstructionState.PROCESSED,
         status=PaymentRecordState.TRANSFERRED_TO_FSP,
     )
-    client = MoneyGramClient()
     if ids:
         qs = qs.filter(id__in=ids)
     for record in qs:
