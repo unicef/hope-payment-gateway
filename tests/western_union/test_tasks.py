ff68db5c7aa3c3f02fbd59312b800adf2151d666
Giorgi Patsatsia <61111267+patsatsia@users.noreply.github.com>
2025-05-09 04:19:38 +0400

    Add tests to cover WU tasks (#214)

--- tests/western_union/test_tasks.py
+++ tests/western_union/test_tasks.py
@@ -4,9 +4,17 @@ import pytest
 from constance.test import override_config
 from django.test import override_settings
 from factories import PaymentInstructionFactory, PaymentRecordFactory
-
-from hope_payment_gateway.apps.fsp.western_union.tasks import western_union_send_task
+from hope_payment_gateway.apps.fsp.western_union.api.client import WesternUnionClient
+from hope_payment_gateway.apps.fsp.western_union.tasks import (
+    western_union_send_task,
+    update_corridors,
+    update_templates,
+    western_union_notify,
+)
 from hope_payment_gateway.apps.gateway.models import PaymentInstructionState, PaymentRecordState
+from strategy_field.utils import fqn
+
+from tests.factories import CorridorFactory
 
 
 @pytest.mark.parametrize(
@@ -44,3 +52,52 @@ def test_send_money_task(mock_class, wu, rec_a, rec_b, total):
 
     western_union_send_task(tag="tag", threshold=10)
     assert len(mock_class.mock_calls) == total
+
+
+@pytest.mark.django_db
+@override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+@patch("hope_payment_gateway.apps.fsp.western_union.tasks.WesternUnionClient")
+def test_update_corridors(mock_client):
+    update_corridors()
+
+    mock_client.return_value.das_countries_currencies.assert_called_once_with(create_corridors=True)
+
+
+@pytest.mark.django_db
+@override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+@patch("hope_payment_gateway.apps.fsp.western_union.tasks.WesternUnionClient")
+def test_update_templates(mock_client):
+    corridor_data = [
+        {
+            "destination_country": "US",
+            "destination_currency": "USD",
+            "template_code": "1234",
+        },
+        {
+            "destination_country": "GB",
+            "destination_currency": "GBP",
+            "template_code": "4321",
+        },
+    ]
+
+    for corr in corridor_data:
+        CorridorFactory.create(**corr)
+
+    update_templates()
+
+    assert mock_client.return_value.das_delivery_option_template.call_count == 2
+
+    for corr in corridor_data:
+        mock_client.return_value.das_delivery_option_template.assert_any_call(*corr.values())
+
+
+@pytest.mark.django_db
+@override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+@patch("hope_payment_gateway.apps.fsp.western_union.tasks.notify_records_to_fsp")
+def test_western_union_notify(mock_notify):
+    records = PaymentRecordFactory.create_batch(3, status=PaymentRecordState.PENDING)
+    record_ids = [record.id for record in records]
+    mock_notify.return_value = None
+    western_union_notify(record_ids)
+
+    mock_notify.assert_called_once_with(fqn(WesternUnionClient), record_ids)
