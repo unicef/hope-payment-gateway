d654a19a14580f17c7f0e246657f2c20786614c5
Domenico DiNicola <dom.dinicola@gmail.com>
2026-01-16 16:29:16 +0100

    cookie alignment

--- src/hope_payment_gateway/apps/core/models.py
+++ src/hope_payment_gateway/apps/core/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from unicef_security.models import AbstractUser, SecurityMixin
+from unicef_security.models import AbstractUser, SecurityMixin, TimeStampedModel
 
 
 class Singleton(type):
@@ -12,7 +12,7 @@ class Singleton(type):
         return cls._instances[cls]
 
 
-class User(SecurityMixin, AbstractUser):
+class User(TimeStampedModel, SecurityMixin, AbstractUser):
     class Meta:
         app_label = "core"
 
